b86f152c76e628811b18bad68b99cbdb3509429a
sleepy-bear <jew8969@gmail.com>
2024-06-18 21:05:29 +0900

    Router 수정

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import {useParams} from "react-router-dom"; //hooks 제공
+import {createSearchParams, useNavigate, useParams, useSearchParams} from "react-router-dom"; //hooks 제공
 
 function ReadPage(props) {
 
+    const navigate = useNavigate();
     const {tno} = useParams()
+
+    const [queryParams] = useSearchParams()
+
+    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+
+    const queryStr = createSearchParams({page:page, size:size}).toString();
+
+
 
     console.log('obj : ' , tno);
 
+    const moveToModify = (tno) => {
+        navigate({pathname: `/todo/modify/${tno}`, search: queryStr})
+    }
+
+    const moveToList = () => {
+        navigate({pathname: `/todo/list/`, search: queryStr})
+    }
+    
     return (
-        <div className={'text-3xl'}> Todo Read Page</div>
+        <div className={'text-3xl'}> Todo Read Page {tno}
+            <div>
+                <button onClick={() => moveToModify(tno)}>Test Modify</button>
+                <button onClick={() => moveToList()}>Test List</button>
+            </div>
+        </div>
     );
 }
 
