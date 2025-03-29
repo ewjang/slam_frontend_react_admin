35fdfcf48e7284b3da308a6b4c73f29bdc36a606
sleepy-bear <jew8969@gmail.com>
2025-03-29 21:39:07 +0900

    상품 조회 컴포넌트 및 라우터 추가

--- src/router/root.js
+++ src/router/root.js
@@ -13,11 +13,15 @@ import {Suspense, lazy} from "react";
 import {createBrowserRouter} from 'react-router-dom'
 import todoRouter from "./todoRouter";
 
+import productsRouter from "./productsRouter";
+
 const Loading = <div className={'bg-red-700'}>Loading....</div>
 const Main = lazy(() => import("../pages/MainPage"))
+
 const About = lazy(() => import("../pages/AboutPage"))
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
 
+const ProductsIndex = lazy(() => import("../pages/products/IndexPage"))
 
 const root = createBrowserRouter([
     {
@@ -33,7 +37,11 @@ const root = createBrowserRouter([
         element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
         children: todoRouter()
     },
-
+    {
+        path: 'products',
+        element: <Suspense fallback={Loading}><ProductsIndex/></Suspense>,
+        children: productsRouter()
+    },
 ])
 
 
